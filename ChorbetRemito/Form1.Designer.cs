9947e619181cae7e928e8bf4806130a51d35da4a
VacaMalenzuela <macarenavalenz@gmail.com>
2023-02-07 20:13:08 -0300

    Add project files.

---
+++ ChorbetRemito/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ChorbetRemito
+{
+    partial class FormRemito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxClientes = new System.Windows.Forms.ComboBox();
+            this.cbxProductos = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.nudCantidad = new System.Windows.Forms.NumericUpDown();
+            this.txtPrecioUni = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxClientes
+            // 
+            this.cbxClientes.FormattingEnabled = true;
+            this.cbxClientes.Location = new System.Drawing.Point(299, 48);
+            this.cbxClientes.Name = "cbxClientes";
+            this.cbxClientes.Size = new System.Drawing.Size(121, 24);
+            this.cbxClientes.TabIndex = 0;
+            // 
+            // cbxProductos
+            // 
+            this.cbxProductos.FormattingEnabled = true;
+            this.cbxProductos.Location = new System.Drawing.Point(299, 87);
+            this.cbxProductos.Name = "cbxProductos";
+            this.cbxProductos.Size = new System.Drawing.Size(121, 24);
+            this.cbxProductos.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(190, 254);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(539, 254);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(95, 50);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(519, 351);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(115, 53);
+            this.btnCrear.TabIndex = 4;
+            this.btnCrear.Text = "Crear Remito";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(190, 51);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(48, 16);
+            this.lblCliente.TabIndex = 5;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(190, 207);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(38, 16);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "Total";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(187, 168);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(61, 16);
+            this.lblCantidad.TabIndex = 7;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(187, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Precio Unitario";
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(190, 90);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(61, 16);
+            this.lblProducto.TabIndex = 9;
+            this.lblProducto.Text = "Producto";
+            // 
+            // nudCantidad
+            // 
+            this.nudCantidad.Location = new System.Drawing.Point(299, 166);
+            this.nudCantidad.Name = "nudCantidad";
+            this.nudCantidad.Size = new System.Drawing.Size(120, 22);
+            this.nudCantidad.TabIndex = 10;
+            // 
+            // txtPrecioUni
+            // 
+            this.txtPrecioUni.Location = new System.Drawing.Point(299, 128);
+            this.txtPrecioUni.Name = "txtPrecioUni";
+            this.txtPrecioUni.Size = new System.Drawing.Size(100, 22);
+            this.txtPrecioUni.TabIndex = 11;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(299, 204);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 22);
+            this.txtTotal.TabIndex = 12;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(539, 83);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(95, 50);
+            this.btnAgregar.TabIndex = 13;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // FormRemito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 522);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtPrecioUni);
+            this.Controls.Add(this.nudCantidad);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cbxProductos);
+            this.Controls.Add(this.cbxClientes);
+            this.Name = "FormRemito";
+            this.Text = "REMITO";
+            this.Load += new System.EventHandler(this.FormRemito_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxClientes;
+        private System.Windows.Forms.ComboBox cbxProductos;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.NumericUpDown nudCantidad;
+        private System.Windows.Forms.TextBox txtPrecioUni;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
+
