ea0d5f3e7b08b958fd62a96180d49f4b48b5181f
VacaMalenzuela <macarenavalenz@gmail.com>
2023-02-07 20:55:00 -0300

    Creacion de las librerias de clases Modelo y Dominio

---
+++ Dominio/ClienteNegocio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Dominio
+{
+    public class ClienteNegocio
+    {
+        public List<Cliente> listar()
+        {
+            List<Cliente> Lista = new List<Cliente>();
+            AccesoDatos datos = new AccesoDatos();
+            datos.setConsulta("select c.id, c.nombre, encargado, telefono, direccion, d.nombre as Departamento from clientes c inner join departamentos d on d.id = c.iddepartamento where activo = 1");
+            datos.EjecutarLectura();
+
+            while (datos.Lector.Read())
+            {
+                Cliente aux = new Cliente();
+
+                aux.Id = (int)datos.Lector["id"];
+                if (!(datos.Lector["c.nombre"] is DBNull))
+                    aux.Nombre = (string)datos.Lector["c.nombre"];
+                if (!(datos.Lector["encargado"] is DBNull))
+                    aux.Encargado = (string)datos.Lector["encargado"];
+                if (!(datos.Lector["telefono"] is DBNull))
+                    aux.Telefono = (string)datos.Lector["telefono"];
+                if (!(datos.Lector["direccion"] is DBNull))
+                    aux.Direccion = (string)datos.Lector["direccion"];
+                if (!(datos.Lector["d.nombre"] is DBNull))
+                    aux.Departamento = (string)datos.Lector["d.nombre"];
+
+                Lista.Add(aux);
+            }
+
+            return Lista;
+        }
+    }
+}
---
+++ Dominio/ProductoNegocio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Dominio
+{
+    public class ProductoNegocio
+    {
+        public List<Producto> listar()
+        {
+            List<Producto> Lista = new List<Producto>();
+            AccesoDatos datos = new AccesoDatos();
+            datos.setConsulta("select ID, Codigo, Nombre, PrecioUni from Productos");
+            datos.EjecutarLectura();
+
+            while (datos.Lector.Read())
+            {
+                Producto aux = new Producto();
+
+                aux.ID = (int)datos.Lector["Id"];
+                if (!(datos.Lector["Codigo"] is DBNull))
+                    aux.codigo = (string)datos.Lector["Codigo"];
+                if (!(datos.Lector["Nombre"] is DBNull))
+                    aux.nombre = (string)datos.Lector["Nombre"];
+                if (!(datos.Lector["PrecioUni"] is DBNull))
+                    aux.precio = (float)datos.Lector["PrecioUni"];
+
+                Lista.Add(aux);
+            }
+
+            return Lista;
+        }
+    }
+}
